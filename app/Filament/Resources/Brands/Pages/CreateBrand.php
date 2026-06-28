c87ead0b8faa4ce40f76c3787969bec4586d6af4
Tùng Đào <daotung492@gmail.com>
2026-06-28 09:40:16 +0700

    feat(04-03): add Translatable concern + LocaleSwitcher to all 5 Create pages
    - Added LaraZeus Translatable trait to CreateCategory, CreateBrand, CreateProduct, CreatePost, CreateProject
    - All Create pages now have getHeaderActions() with LocaleSwitcher::make()
    - CreateProduct retains its existing afterSave() image resize logic

--- app/Filament/Resources/Brands/Pages/CreateBrand.php
+++ app/Filament/Resources/Brands/Pages/CreateBrand.php
@@ -4,8 +4,19 @@ namespace App\Filament\Resources\Brands\Pages;
 
 use App\Filament\Resources\Brands\BrandResource;
 use Filament\Resources\Pages\CreateRecord;
+use LaraZeus\SpatieTranslatable\Actions\LocaleSwitcher;
+use LaraZeus\SpatieTranslatable\Resources\Pages\CreateRecord\Concerns\Translatable;
 
 class CreateBrand extends CreateRecord
 {
+    use Translatable;
+
     protected static string $resource = BrandResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            LocaleSwitcher::make(),
+        ];
+    }
 }
--- app/Filament/Resources/Products/Pages/CreateProduct.php
+++ app/Filament/Resources/Products/Pages/CreateProduct.php
@@ -6,10 +6,21 @@ use App\Filament\Resources\Products\ProductResource;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Laravel\Facades\Image;
+use LaraZeus\SpatieTranslatable\Actions\LocaleSwitcher;
+use LaraZeus\SpatieTranslatable\Resources\Pages\CreateRecord\Concerns\Translatable;
 
 class CreateProduct extends CreateRecord
 {
+    use Translatable;
+
     protected static string $resource = ProductResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            LocaleSwitcher::make(),
+        ];
+    }
 
     protected function afterSave(): void
     {
